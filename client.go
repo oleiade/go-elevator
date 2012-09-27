baeb5da78f858fedeb8d3dda7f0762d6d9c7f931
Theo Crevon <tcrevon@gmail.com>
2012-09-27 14:01:30 -0700

    Update : Send messages ability

--- client.go
+++ client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+    "fmt"
+    "log"
     "bytes"
     "github.com/ugorji/go-msgpack"
-    "github.com/alecthomas/gozmq"
+    zmq "github.com/alecthomas/gozmq"
 )
 
 type Request struct {
@@ -24,5 +26,47 @@ func packRequest(r *Request) (*bytes.Buffer) {
 
     return buffer
 }
+
+func newMessage(r *Request) ([][]byte) {
+    var preq *bytes.Buffer = packRequest(r)
+    var breq byte
+
+    breq, err := preq.ReadByte()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(breq)
+
+    return [][]byte{[]byte{breq}}
+}
+
+func send(s zmq.Socket, r *Request) ([][]byte) {
+    msg := newMessage(r)
+    err := s.SendMultipart(msg, 0)
+    
+    fmt.Println(msg)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    parts, _ := s.RecvMultipart(0)
+
+    return parts
+}
+
 func main() {
+    req := &Request{
+        db_uid: "a9032698-12f8-45a6-ab3e-0e00915ed700",
+        command: "GET",
+        args: []string{"1"},
+    }
+
+    context, _ := zmq.NewContext()
+    defer context.Close()
+    socket, _ := context.NewSocket(zmq.XREQ)
+    socket.Connect("tcp://127.0.0.1:4141")
+
+    res := send(socket, req)
+    fmt.Println(res)
 }
